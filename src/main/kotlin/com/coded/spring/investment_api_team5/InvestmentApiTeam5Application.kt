2105a3516f91e4d0dc2bbcf1b2239683f4291734
Yousef Alothman <yousefoalothman@gmail.com>
2025-05-04 17:02:22 +0300

    Initialize Spring Boot project for Investment API Team 5
    - Set up base project using Spring Initializr
    - Tech stack: Kotlin, Spring Boot 3.4.4, Maven, Java 21
    - Group: com.coded.spring
    - Artifact: investment-api-team5
    - Base package: com.coded.spring.investment_api_team5

---
+++ src/main/kotlin/com/coded/spring/investment_api_team5/InvestmentApiTeam5Application.kt
@@ -0,0 +1,11 @@
+package com.coded.spring.investment_api_team5
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InvestmentApiTeam5Application
+
+fun main(args: Array<String>) {
+    runApplication<InvestmentApiTeam5Application>(*args)
+}
